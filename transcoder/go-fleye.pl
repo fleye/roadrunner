442efb9e5cbb8ff12dad93f5b98297a962f2c9be
Tom Daly <tjd@q7.io>
2013-03-26 02:27:17 +0000

    Worker fun!

---
+++ transcoder/go-fleye.pl
@@ -0,0 +1,134 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use constant DEBUG => 1;
+
+use Data::Dumper;
+use Config::General;
+use Cwd;
+use Digest::MD5 qw(md5_base64);
+use Gearman::Client;
+use Storable qw(freeze);
+
+# Read in configuration block
+my $conf = new Config::General("/home/ec2-user/fleye/transcoder/go-fleye.conf");
+my %config = $conf->getall;
+
+print "Configuration Dump:\n" if DEBUG;
+print Dumper %config if DEBUG;
+
+# Connect out to Gearmand
+my $gearmand = Gearman::Client->new();
+my $server = $config{'fleye_gearmand'};
+$server .= ":4730";
+$gearmand->job_servers('127.0.0.1:4730') || die "Didn't connect to gearmand\n";
+
+print Dumper $gearmand;
+
+# Locate the current working directory, which should be a mounted SDXC card, and generate an MD5 of the path.
+my $cwd = getcwd();
+my $dir_md5 = md5_base64($cwd . time());
+
+print "Working Dir: $cwd\n" if DEBUG;
+print "MD5 Dir: $dir_md5\n" if DEBUG;
+
+# Send a job to gearmand that sets up the remote paths for this card. We do this synchronously because we want to block to make sure the paths get created.
+
+my %paths;
+
+$paths{'storage_path'} = $config{'storage_path'};
+$paths{'card_id'} = $dir_md5;
+
+my $args = freeze \%paths;
+
+my $task = Gearman::Task->new("process_card", \$args, {
+	high_priority => 1,
+	});
+
+print "Kicking off Gearman job to process_card...\n";	
+$gearmand->do_task($task);
+
+# Find all of the LRV files, and create a hash of the files and their remote files names (which includes created time)
+
+my %lrv_files;
+
+$lrv_files{'hostname'} = $config{'local_hostname'};
+$lrv_files{'src_path'} = $cwd;
+$lrv_files{'storage_path'} = $config{'storage_path'};
+$lrv_files{'card_id'} = $dir_md5;
+
+my @lrv_files = glob("*.lrv");
+
+foreach my $lrv (sort @lrv_files) {
+	my $file_ctime_epoch = (stat($lrv))[10];
+	my @ctime = localtime($file_ctime_epoch);
+	
+	my $ctime_year = $ctime[5]+1900;
+	my $ctime_month = sprintf("%02d", $ctime[4]+1);
+	my $ctime_day = sprintf("%02d", $ctime[3]);
+	my $ctime_hour = sprintf("%02d", $ctime[2]);
+	my $ctime_min = sprintf("%02d", $ctime[1]);
+	my $ctime_sec = sprintf("%02d", $ctime[0]);
+	
+	my ($base) = split(/\./, $lrv);
+	
+	my $renamed_file = "$base-$ctime_year-$ctime_month-$ctime_day-$ctime_hour-$ctime_min-$ctime_sec-240p.mp4";
+	
+	$lrv_files{$lrv} = $renamed_file;
+	
+}
+
+print "Showing LRV files\n" if DEBUG;
+print Dumper %lrv_files if DEBUG;
+
+my $args = freeze \%lrv_files;
+
+my $task = Gearman::Task->new("copy_lrv", \$args, {
+	high_priority => 1,
+	});
+	
+$gearmand->dispatch_background($task);
+
+# Find all of the MP4 files, and create a hash of the files and their remote files names (which includes created time)
+
+my %mp4_files;
+
+$mp4_files{'hostname'} = $config{'local_hostname'};
+$mp4_files{'src_path'} = $cwd;
+$mp4_files{'storage_path'} = $config{'storage_path'};
+$mp4_files{'card_id'} = $dir_md5;
+
+my @mp4_files = glob("*.mp4");
+
+foreach my $mp4 (sort @mp4_files) {
+	my $file_ctime_epoch = (stat($mp4))[10];
+	my @ctime = localtime($file_ctime_epoch);
+	
+	my $ctime_year = $ctime[5]+1900;
+	my $ctime_month = sprintf("%02d", $ctime[4]+1);
+	my $ctime_day = sprintf("%02d", $ctime[3]);
+	my $ctime_hour = sprintf("%02d", $ctime[2]);
+	my $ctime_min = sprintf("%02d", $ctime[1]);
+	my $ctime_sec = sprintf("%02d", $ctime[0]);
+	
+	my ($base) = split(/\./, $mp4);
+	
+	my $renamed_file = "$base-$ctime_year-$ctime_month-$ctime_day-$ctime_hour-$ctime_min-$ctime_sec-1080p.mp4";
+	
+	$mp4_files{$mp4} = $renamed_file;
+	
+}
+
+print "Showing MP4 files\n" if DEBUG;
+print Dumper %mp4_files if DEBUG;
+
+my $args = freeze \%mp4_files;
+
+my $task = Gearman::Task->new("transcode_1080p_360p", \$args, {
+	});
+	
+$gearmand->dispatch_background($task);
+
+
---
+++ transcoder/transcoding_worker.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Gearman::Worker;
+use Storable qw (thaw);
+use Data::Dumper;
+
+my $worker = Gearman::Worker->new();
+
+$worker->job_servers('127.0.0.1:4730') || die "Could not connect to job server\n";
+
+$worker->register_function(
+	process_card => \&process_card
+);
+
+$worker->register_function(
+	copy_lrv => \&copy_lrv
+);
+
+$worker->register_function(
+	transcode_1080p_360p => \&transcode_1080p_360p
+);
+
+$worker->work while 1;
+
+sub process_card {
+	print "Received process_card job.\n";
+	my %args = %{thaw($_[0]->arg)};
+	print Dumper %args;
+
+	my $storage_path = $args{'storage_path'};
+	my $card_id = $args{'card_id'};
+	
+	system("mkdir $storage_path\/$card_id");
+	system("mkdir $storage_path\/$card_id\/240p");
+	system("mkdir $storage_path\/$card_id\/360p");
+	system("mkdir $storage_path\/$card_id\/1080p");	
+}
+
+
+sub copy_lrv {
+	print "Received copy_lrv job.\n";
+	my %args = %{thaw($_[0]->arg)};
+	print Dumper %args;
+
+	my $remote_hostname = delete $args{'hostname'};
+	my $src_path = delete $args{'src_path'};
+	my $storage_path = delete $args{'storage_path'};
+	my $card_id = delete $args{'card_id'};
+		
+	foreach my $lrv (sort keys %args){
+		next unless $lrv =~ m/.+\.lrv/;
+		my $dst_file = $args{$lrv};
+
+		print "Copying from $remote_hostname $src_path $lrv to $storage_path $card_id 240p $dst_file\n";
+		system("/usr/bin/scp ec2-user\@$remote_hostname\:$src_path\/$lrv $storage_path\/$card_id\/240p\/$dst_file");
+	}
+}
+
+sub transcode_1080p_360p {
+	print "Received transcode_1080p_360p job.\n";
+	my %args = %{thaw($_[0]->arg)};
+	print Dumper %args;
+	
+	my $remote_hostname = delete $args{'hostname'};
+	my $src_path = delete $args{'src_path'};
+	my $storage_path = delete $args{'storage_path'};
+	my $card_id = delete $args{'card_id'};
+
+	foreach my $mp4 (sort keys %args){
+		next unless $mp4 =~ m/.+\.mp4/;
+		my $dst_file = $args{$mp4};
+
+		print "Copying from $remote_hostname $src_path $mp4 to $storage_path $card_id 1080p $dst_file\n";
+		system("/usr/bin/scp ec2-user\@$remote_hostname\:$src_path\/$mp4 $storage_path\/$card_id\/1080p\/$dst_file");	
+			
+		print "Asynchronously kicking off transcoding job\n";
+		system("cp $storage_path\/$card_id\/1080p\/$dst_file $storage_path\/$card_id\/360p\/$dst_file")
+		}
+}
